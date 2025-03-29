13314536e6e6269f210522fca1329952d9246097
Imane Taruf <imane93100@hotmail.fr>
2025-03-29 23:52:25 +0900

    Add an Hit Event Component with an Event Dispatcher with 2 params

---
+++ Source/ProjectSkyRunner/Components/HitEventComponent.cpp
@@ -0,0 +1,11 @@
+﻿#include "HitEventComponent.h"
+
+UHitEventComponent::UHitEventComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UHitEventComponent::NotifyHit(AActor* InstigatingActor, FVector HitLocation)
+{
+	OnHit.Broadcast(InstigatingActor, HitLocation);
+}
---
+++ Source/ProjectSkyRunner/Components/HitEventComponent.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HitEventComponent.generated.h"
+
+// Declare a dynamic multicast delegate for hit events with the instigating actor and hit location.
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnHitEvent, AActor*, InstigatingActor, FVector, HitLocation);
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class PROJECTSKYRUNNER_API UHitEventComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	UHitEventComponent();
+
+	// Event dispatcher that is broadcast when a hit occurs.
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+	FOnHitEvent OnHit;
+
+	// Call this function to notify listeners about a hit.
+	UFUNCTION(BlueprintCallable, Category = "Events")
+	void NotifyHit(AActor* InstigatingActor, FVector HitLocation);
+};
