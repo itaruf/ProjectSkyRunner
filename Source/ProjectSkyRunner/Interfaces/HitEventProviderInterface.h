01301460a1dc177549f76237bfe9629323b73e82
Imane Taruf <imane93100@hotmail.fr>
2025-03-29 23:58:45 +0900

    Add HitEventProviderInterface to expose HitEventComponent for hit event dispatching.

---
+++ Source/ProjectSkyRunner/Interfaces/HitEventProviderInterface.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "../Components/HitEventComponent.h" // Include the component header so it is available in the interface
+#include "HitEventProviderInterface.generated.h"
+
+UINTERFACE(Blueprintable)
+class PROJECTSKYRUNNER_API UHitEventProviderInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * Interface for providing a HitEventComponent.
+ */
+class PROJECTSKYRUNNER_API IHitEventProviderInterface
+{
+	GENERATED_BODY()
+
+public:
+	/**
+	 * Returns the HitEventComponent associated with this actor.
+	 */
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Hit")
+	UHitEventComponent* GetHitEventComponent() const;
+};
