ee44f7665c44584e2db9e9c145351f631840f364
Imane Taruf <imane93100@hotmail.fr>
2025-03-30 15:23:13 +0900

    Add DamageTypeEx.h to define extended damage types

---
+++ Source/ProjectSkyRunner/Enumerations/DamageTypeEx.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+
+UENUM(BlueprintType)
+enum class EDamageType : uint8
+{
+	Physical UMETA(DisplayName = "Physical"),
+	Fire UMETA(DisplayName = "Fire"),
+	Ice UMETA(DisplayName = "Ice"),
+	// Add additional damage types as needed.
+};
