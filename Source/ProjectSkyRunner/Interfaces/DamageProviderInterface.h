b247e50ed4a66ff34d053dc12c011cdb1f54e4fa
Imane Taruf <imane93100@hotmail.fr>
2025-03-30 17:05:04 +0900

    Add DamageProviderInterface to expose DamageComponent via interface

---
+++ Source/ProjectSkyRunner/Interfaces/DamageProviderInterface.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "../Components/DamageComponent.h"
+#include "DamageProviderInterface.generated.h"
+
+UINTERFACE(Blueprintable)
+class PROJECTSKYRUNNER_API UDamageProviderInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * Interface for providing a DamageComponent.
+ */
+class PROJECTSKYRUNNER_API IDamageProviderInterface
+{
+	GENERATED_BODY()
+
+public:
+	/**
+	 * Returns the DamageComponent associated with this actor.
+	 */
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Damage")
+	UDamageComponent* GetDamageComponent() const;
+};
