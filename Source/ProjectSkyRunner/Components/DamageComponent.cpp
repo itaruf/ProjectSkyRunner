aeb003242ae1219570489b772329b52cfcd67048
Imane Taruf <imane93100@hotmail.fr>
2025-03-30 17:02:20 +0900

    Implement DamageComponent to handle damage-focused logic

---
+++ Source/ProjectSkyRunner/Components/DamageComponent.cpp
@@ -0,0 +1,43 @@
+﻿#include "DamageComponent.h"
+#include "../Interfaces/DamageableInterface.h"
+#include "GameFramework/Actor.h"
+
+UDamageComponent::UDamageComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+
+	// Set default damage values.
+	DamageData.DamageAmount = 10.f;
+	DamageData.DamageType = EDamageType::Physical;
+	DamageData.HitImpulse = FVector::ZeroVector;
+	DamageData.HitBone = NAME_None;
+	DamageData.bCriticalHit = false;
+	// InstigatingActor, HitLocation, DamageCauser, and TimeStamp will be set at runtime.
+}
+
+void UDamageComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void UDamageComponent::DealDamage(AActor* Target)
+{
+	if (!Target)
+	{
+		return;
+	}
+
+	// Check if the target implements the damageable interface.
+	if (Target->Implements<UDamageableInterface>())
+	{
+		// Create a copy of the default damage data and fill in runtime details.
+		FDamageData DamageToApply = DamageData;
+		DamageToApply.InstigatingActor = GetOwner();
+		DamageToApply.HitLocation = Target->GetActorLocation();
+		DamageToApply.DamageCauser = GetOwner();
+		DamageToApply.TimeStamp = GetWorld() ? GetWorld()->GetTimeSeconds() : 0.f;
+
+		// Apply damage using the damage amount from the structure.
+		IDamageableInterface::Execute_TakeDamage(Target, DamageToApply.DamageAmount);
+	}
+}
---
+++ Source/ProjectSkyRunner/Components/DamageComponent.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "../Structures/DamageData.h"
+#include "DamageComponent.generated.h"
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class PROJECTSKYRUNNER_API UDamageComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UDamageComponent();
+
+	// Default damage values stored in a structured format.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage")
+	FDamageData DamageData;
+
+	// Deals damage to the specified target.
+	UFUNCTION(BlueprintCallable, Category = "Damage")
+	void DealDamage(AActor* Target);
+
+protected:
+	virtual void BeginPlay() override;
+};
+
