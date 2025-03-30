d0bd789893cec40ef2f0ac8b0dd0d63cc3c685c6
Imane Taruf <imane93100@hotmail.fr>
2025-03-30 19:01:12 +0900

    Add UMeleeDamageDealerComponent to apply a melee damage multiplier

---
+++ Source/ProjectSkyRunner/Components/MeleeDamageDealerComponent.cpp
@@ -0,0 +1,14 @@
+﻿#include "MeleeDamageDealerComponent.h"
+#include "GameFramework/Actor.h"
+
+UMeleeDamageDealerComponent::UMeleeDamageDealerComponent()
+{
+	// Set default melee multiplier.
+	MeleeMultiplier = 1.5f;
+}
+
+float UMeleeDamageDealerComponent::CalculateDamage(const FDamageData& InDamageData, AActor* Target) const
+{
+	// Multiply the base damage by the melee multiplier.
+	return InDamageData.DamageAmount * MeleeMultiplier;
+}
---
+++ Source/ProjectSkyRunner/Components/MeleeDamageDealerComponent.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "DamageDealerComponent.h"
+#include "MeleeDamageDealerComponent.generated.h"
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class PROJECTSKYRUNNER_API UMeleeDamageDealerComponent : public UDamageDealerComponent
+{
+	GENERATED_BODY()
+
+public:
+	UMeleeDamageDealerComponent();
+
+protected:
+	// Override to apply a melee-specific damage multiplier.
+	virtual float CalculateDamage(const FDamageData& InDamageData, AActor* Target) const override;
+
+	// Multiplier to scale melee damage.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage|Melee")
+	float MeleeMultiplier;
+};
