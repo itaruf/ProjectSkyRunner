5ed3b89904c995186e8837277c7061972d9b6e85
Imane Taruf <imane93100@hotmail.fr>
2025-04-12 16:17:32 +0900

    Add an Actor to test how gameplay classes can access services

---
+++ Source/ProjectSkyRunner/Actors/MyActor.cpp
@@ -0,0 +1,36 @@
+﻿// MyActor.cpp
+
+#include "MyActor.h"
+#include "../Audio/Services/AudioService.h"
+#include "../Core/ServiceLocator/GameServiceLocator.h"
+#include "../Inventory/Services/InventoryService.h"
+#include "Sound/SoundBase.h"
+
+void AMyActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Attempt to retrieve the Inventory Service from the Service Locator
+	// This allows us to interact with the inventory system without having
+	// to maintain a direct reference to it
+	if (UInventoryService* InventoryService = UGameServiceLocator::GetService<UInventoryService>("Inventory"))
+	{
+		// Add one HealthPotion to the inventory
+		InventoryService->AddItem("HealthPotion", 1);
+
+		// Retrieve and log the current count of HealthPotions in the inventory
+		int32 Count = InventoryService->GetItemCount("HealthPotion");
+		UE_LOG(LogTemp, Log, TEXT("HealthPotion Count: %d"), Count);
+	}
+
+	// Attempt to retrieve the Audio Service
+	// This allows us to play a sound without depending on the audio system directly
+	if (UAudioService* AudioService = UGameServiceLocator::GetService<UAudioService>("Audio"))
+	{
+		// If a sound has been assigned in the editor or Blueprint, play it at the Actor's location
+		if (MySound)
+		{
+			AudioService->PlaySoundAtLocation(MySound, GetActorLocation());
+		}
+	}
+}
---
+++ Source/ProjectSkyRunner/Actors/MyActor.h
@@ -0,0 +1,27 @@
+﻿// MyActor.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MyActor.generated.h"
+
+class USoundBase;
+
+// This example Actor demonstrates how gameplay classes can access services
+// like Inventory or Audio through the Service Locator without direct dependencies
+UCLASS()
+class PROJECTSKYRUNNER_API AMyActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Called when the actor is first placed or spawned in the world
+	// We'll use this to retrieve and interact with services
+	virtual void BeginPlay() override;
+
+	// A sound asset reference that can be set from the editor or Blueprint
+	// This sound will be played via the AudioService when the game starts
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Audio")
+	USoundBase* MySound;
+};
