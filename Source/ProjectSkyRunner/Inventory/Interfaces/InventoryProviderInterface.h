58e95e721ae1ba513b7bf10d30a558159deb7647
Imane Taruf <imane93100@hotmail.fr>
2025-04-12 18:28:13 +0900

    Add an InventoryProviderInterface to get an InventoryComponent from a contractee

---
+++ Source/ProjectSkyRunner/Inventory/Interfaces/InventoryProviderInterface.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "../Components/InventoryComponent.h"
+#include "InventoryProviderInterface.generated.h"
+
+UINTERFACE(Blueprintable)
+class PROJECTSKYRUNNER_API UInventoryProviderInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * Interface for providing a InventoryComponent
+ */
+class PROJECTSKYRUNNER_API IInventoryProviderInterface
+{
+	GENERATED_BODY()
+
+public:
+	/**
+	 * Returns the InventoryComponent associated with this actor
+	 */
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Inventory")
+	UInventoryComponent* GetInventoryComponent() const;
+};
