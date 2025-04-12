e31757623ba135ccbe67e2763060519c484e9b84
Imane Taruf <imane93100@hotmail.fr>
2025-04-12 16:15:03 +0900

    Add a custom GameInstance to manage global services used throughout the game

---
+++ Source/ProjectSkyRunner/Game/SkyRunnerGameInstance.cpp
@@ -0,0 +1,50 @@
+﻿// SkyRunnerGameInstance.cpp
+
+#include "SkyRunnerGameInstance.h"
+#include "../Audio/Services/AudioService.h"
+#include "../Core/ServiceLocator/GameServiceLocator.h"
+#include "../Inventory/Services/InventoryService.h"
+
+void USkyRunnerGameInstance::Init_Implementation()
+{
+	Super::Init();
+
+	// Create an instance of the Inventory Service and register it
+	// This makes the inventory system globally accessible throughout the game
+	UInventoryService* InventoryService = NewObject<UInventoryService>(this, UInventoryService::StaticClass());
+	if (InventoryService)
+	{
+		IGameServiceInterface::Execute_InitializeService(InventoryService); // Prepare the service (e.g., clear internal state)
+		UGameServiceLocator::RegisterService("Inventory", InventoryService);
+	}
+
+	// Create an instance of the Audio Service and register it
+	// Enables any Actor or system to request sound playback globally
+	UAudioService* AudioService = NewObject<UAudioService>(this, UAudioService::StaticClass());
+	if (AudioService)
+	{
+		IGameServiceInterface::Execute_InitializeService(AudioService); // Prepare any audio-related setup if needed
+		UGameServiceLocator::RegisterService("Audio", AudioService);
+	}
+}
+
+void USkyRunnerGameInstance::Shutdown_Implementation()
+{
+	// Gracefully shut down all services before clearing the registry
+	// This allows each service to clean up any resources it holds
+
+	if (UInventoryService* InventoryService = UGameServiceLocator::GetService<UInventoryService>("Inventory"))
+	{
+		IGameServiceInterface::Execute_ShutdownService(InventoryService);
+	}
+
+	if (UAudioService* AudioService = UGameServiceLocator::GetService<UAudioService>("Audio"))
+	{
+		IGameServiceInterface::Execute_ShutdownService(AudioService);
+	}
+
+	// Remove all service registrations to ensure a clean shutdown
+	UGameServiceLocator::ClearServices();
+
+	Super::Shutdown();
+}
---
+++ Source/ProjectSkyRunner/Game/SkyRunnerGameInstance.h
@@ -0,0 +1,30 @@
+﻿// SkyRunnerGameInstance.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "SkyRunnerGameInstance.generated.h"
+
+// This custom GameInstance class is responsible for setting up
+// and tearing down global services used throughout the game
+// It provides a centralized location to register services
+// with the Service Locator during initialization
+UCLASS()
+class PROJECTSKYRUNNER_API USkyRunnerGameInstance: public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+	// Called once at the very start of the game, before any levels are loaded.
+	// Marked as BlueprintNativeEvent to allow Blueprint overrides.
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "GameInstance")
+	void Init() override;
+	virtual void Init_Implementation();
+
+	// Called when the game is shutting down.
+	// Marked as BlueprintNativeEvent to allow Blueprint overrides.
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "GameInstance")
+	void Shutdown() override;
+	virtual void Shutdown_Implementation();
+};
