1aed1244cf41d11626217e278f7a97f6eaf39567
Imane Taruf <imane93100@hotmail.fr>
2025-04-13 02:42:15 +0900

    Create a custom GravityCharacterMovementComponent to handle gravity-based movement logic

---
+++ Source/ProjectSkyRunner/Movements/Gravity/GravityCharacterMovementComponent.cpp
@@ -0,0 +1,41 @@
+﻿#include "GravityCharacterMovementComponent.h"
+
+UGravityCharacterMovementComponent::UGravityCharacterMovementComponent()
+{
+	bGravityMode = false;
+}
+
+void UGravityCharacterMovementComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	OriginalGravityScale = GravityScale;  // Store original gravity scale
+}
+
+void UGravityCharacterMovementComponent::ToggleGravityMode()
+{
+	bGravityMode = !bGravityMode;
+
+	if (bGravityMode)
+	{
+		GravityScale = 0.f;              // Disable gravity
+		Velocity = FVector::ZeroVector;  // Immediately stop movement
+		SetMovementMode(MOVE_Falling);   // Ensure we're in falling mode to allow floating
+	}
+	else
+	{
+		GravityScale = OriginalGravityScale;  // Restore gravity
+		SetMovementMode(MOVE_Falling);        // Return to standard falling mode
+	}
+}
+
+void UGravityCharacterMovementComponent::PhysFalling(float deltaTime, int32 Iterations)
+{
+	if (bGravityMode)
+	{
+		Velocity = FVector::ZeroVector; // Completely stationary when in gravity mode
+		return;                         // Skip default gravity physics
+	}
+
+	Super::PhysFalling(deltaTime, Iterations);
+}
---
+++ Source/ProjectSkyRunner/Movements/Gravity/GravityCharacterMovementComponent.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GravityCharacterMovementComponent.generated.h"
+
+/**
+ * Custom movement component for gravity toggle (floating) mode.
+ */
+UCLASS()
+class PROJECTSKYRUNNER_API UGravityCharacterMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+
+public:
+	UGravityCharacterMovementComponent();
+
+	// Indicates whether gravity mode (floating) is active
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Gravity Mode")
+	bool bGravityMode;
+
+	// Original gravity scale to restore after toggling gravity mode off
+	float OriginalGravityScale;
+
+	// Toggles gravity mode on/off
+	UFUNCTION(BlueprintCallable)
+	void ToggleGravityMode();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void PhysFalling(float deltaTime, int32 Iterations) override;
+};
