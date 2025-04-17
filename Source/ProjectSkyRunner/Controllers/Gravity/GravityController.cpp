cc28d260d0a69b5327cf063d3aaea0914a530db6
Imane Taruf <imane93100@hotmail.fr>
2025-04-17 22:14:32 +0900

    (GravityController): Bring Gravity-based actions + Implement ControllerInterface to setup bindings

--- Source/ProjectSkyRunner/Controllers/Gravity/GravityController.cpp
+++ Source/ProjectSkyRunner/Controllers/Gravity/GravityController.cpp
@@ -1,17 +1,24 @@
 #include "GravityController.h"
+
+#include "EnhancedInputComponent.h"
 #include "GameFramework/Character.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "../../Movements/Gravity/GravityCharacterMovementComponent.h"
+
+void AGravityController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	GravityCharacter = Cast<ACharacter>(GetPawn());
+	if (GravityCharacter)
+	{
+		GravityCharacterMovement = Cast<UGravityCharacterMovementComponent>(GravityCharacter->GetCharacterMovement());
+	}
+}
 
 void AGravityController::UpdateRotation(float DeltaTime)
 {
-	FVector GravityDirection = FVector::DownVector;
-	if (ACharacter* PlayerCharacter = Cast<ACharacter>(GetPawn()))
-	{
-		if (UCharacterMovementComponent* MoveComp = PlayerCharacter->GetCharacterMovement())
-		{
-			GravityDirection = MoveComp->GetGravityDirection();
-		}
-	}
+	const FVector GravityDirection = GravityCharacterMovement->GetGravityDirection();
 
 	// Get the current control rotation in world space
 	FRotator ViewRotation = GetControlRotation();
@@ -23,7 +30,7 @@ void AGravityController::UpdateRotation(float DeltaTime)
 		const FQuat DeltaGravityRotation = FQuat::FindBetweenNormals(LastFrameGravity, GravityDirection);
 		const FQuat WarpedCameraRotation = DeltaGravityRotation * FQuat(ViewRotation);
 
-		ViewRotation = WarpedCameraRotation.Rotator();	
+		ViewRotation = WarpedCameraRotation.Rotator();
 	}
 	LastFrameGravity = GravityDirection;
 
@@ -36,8 +43,6 @@ void AGravityController::UpdateRotation(float DeltaTime)
 
 	if (PlayerCameraManager)
 	{
-		ACharacter* PlayerCharacter = Cast<ACharacter>(GetPawn());
-
 		PlayerCameraManager->ProcessViewRotation(DeltaTime, ViewRotation, DeltaRot);
 
 		// Zero the roll of the camera as we always want it horizontal in relation to the gravity.
@@ -47,31 +52,83 @@ void AGravityController::UpdateRotation(float DeltaTime)
 		SetControlRotation(GetGravityWorldRotation(ViewRotation, GravityDirection));
 	}
 
-	APawn* const P = GetPawnOrSpectator();
-	if (P)
-	{
-		P->FaceRotation(ViewRotation, DeltaTime);
-	}
+	GravityCharacter->FaceRotation(ViewRotation, DeltaTime);
 }
 
-FRotator AGravityController::GetGravityRelativeRotation(FRotator Rotation, FVector GravityDirection)
+FRotator AGravityController::GetGravityRelativeRotation(const FRotator& Rotation, const FVector& GravityDirection)
 {
 	if (!GravityDirection.Equals(FVector::DownVector))
 	{
-		FQuat GravityRotation = FQuat::FindBetweenNormals(GravityDirection, FVector::DownVector);
+		const FQuat GravityRotation = FQuat::FindBetweenNormals(GravityDirection, FVector::DownVector);
 		return (GravityRotation * Rotation.Quaternion()).Rotator();
 	}
 
 	return Rotation;
 }
 
-FRotator AGravityController::GetGravityWorldRotation(FRotator Rotation, FVector GravityDirection)
+FRotator AGravityController::GetGravityWorldRotation(const FRotator& Rotation, const FVector& GravityDirection)
 {
 	if (!GravityDirection.Equals(FVector::DownVector))
 	{
-		FQuat GravityRotation = FQuat::FindBetweenNormals(FVector::DownVector, GravityDirection);
+		const FQuat GravityRotation = FQuat::FindBetweenNormals(FVector::DownVector, GravityDirection);
 		return (GravityRotation * Rotation.Quaternion()).Rotator();
 	}
 
 	return Rotation;
 }
+
+void AGravityController::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		if (GravityShiftAction)
+		{
+			EnhancedInputComponent->BindAction(GravityShiftAction, ETriggerEvent::Triggered, this, &AGravityController::GravityShift);
+		}
+		if (GravityReturnAction)
+		{
+			EnhancedInputComponent->BindAction(GravityReturnAction, ETriggerEvent::Triggered, this, &AGravityController::GravityReturn);
+		}
+		if (GravityDiveAction)
+		{
+			EnhancedInputComponent->BindAction(GravityDiveAction, ETriggerEvent::Triggered, this, &AGravityController::GravityDive);
+		}
+	}
+}
+
+void AGravityController::GravityShift()
+{
+	if (GravityCharacter)
+	{
+		if (GravityCharacterMovement)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("GravityShift: Restored normal gravity"));
+		}
+	}
+}
+
+void AGravityController::GravityReturn()
+{
+	if (GravityCharacter)
+	{
+		if (GravityCharacterMovement)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("GravityReturn: Restored normal gravity"));
+		}
+	}
+}
+
+void AGravityController::GravityDive()
+{
+	if (GravityCharacter)
+	{
+		if (GravityCharacterMovement)
+		{
+			if (GravityCharacterMovement->bIsFloating && !GravityCharacterMovement->bIsDiving)
+			{
+				UE_LOG(LogTemp, Warning, TEXT("GravityDive: Starting dive"));
+			}
+		}
+	}
+}
+
--- Source/ProjectSkyRunner/Controllers/Gravity/GravityController.h
+++ Source/ProjectSkyRunner/Controllers/Gravity/GravityController.h
@@ -2,28 +2,59 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "../../Movements/Gravity/GravityCharacterMovementComponent.h"
+#include "../../Controllers/Interfaces/ControllerInterface.h"
 #include "GravityController.generated.h"
 
+class UInputAction;
+struct FInputActionValue;
 /**
  * A Player Controller class which adds input-handling functionality for
  * CharacterMovementController's custom gravity mechanics.
  */
 UCLASS()
-class AGravityController : public APlayerController
+class AGravityController : public APlayerController, public IControllerInterface
 {
 	GENERATED_BODY()
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* GravityShiftAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* GravityReturnAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* GravityDiveAction;
+	
 public:
+	virtual void BeginPlay() override;
+	
 	virtual void UpdateRotation(float DeltaTime) override;
 
 	// Converts a rotation from world space to gravity relative space.
 	UFUNCTION(BlueprintPure)
-	static FRotator GetGravityRelativeRotation(FRotator Rotation, FVector GravityDirection);
+	static FRotator GetGravityRelativeRotation(const FRotator& Rotation, const FVector& GravityDirection);
 
 	// Converts a rotation from gravity relative space to world space.
 	UFUNCTION(BlueprintPure)
-	static FRotator GetGravityWorldRotation(FRotator Rotation, FVector GravityDirection);
+	static FRotator GetGravityWorldRotation(const FRotator& Rotation, const FVector& GravityDirection);
 
+	// --- New functions to control gravity actions ---
+	UFUNCTION(BlueprintCallable)
+	void GravityShift();
+
+	UFUNCTION(BlueprintCallable)
+	void GravityReturn();
+
+	UFUNCTION(BlueprintCallable)
+	void GravityDive();
 private:
+	//
 	FVector LastFrameGravity = FVector::ZeroVector;
+	//
+	ACharacter* GravityCharacter = nullptr;
+	//
+	UGravityCharacterMovementComponent* GravityCharacterMovement = nullptr;
+
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
 };
