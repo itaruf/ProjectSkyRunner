3b8c821f25c6447e32cf889c1b9bef59c155d502
Imane Taruf <imane93100@hotmail.fr>
2025-04-17 22:11:41 +0900

    Implement an interface to share core functionalities between controllers

---
+++ Source/ProjectSkyRunner/Controllers/Interfaces/ControllerInterface.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "ControllerInterface.generated.h"
+
+class UInputComponent;
+
+UINTERFACE(BlueprintType)
+class UControllerInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * Interface to share core functionalities between controllers
+ */
+class PROJECTSKYRUNNER_API IControllerInterface
+{
+	GENERATED_BODY()
+
+public:
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) = 0;
+};
